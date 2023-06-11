29e5c247ef1d418b2f9b63309608791d3d55aa37
Tyler Miller <tyguymills@gmail.com>
2023-06-11 18:48:14 -0500

    Pushing to Heroku

--- Develop/client/src/js/editor.js
+++ Develop/client/src/js/editor.js
@@ -25,8 +25,13 @@ export default class {
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      const localData = localStorage.getItem('content');
+
+      console.info('Loaded data from IndexedDB, injecting into editor', localData);
+      console.log("test", localStorage.getItem('content'));
+      
+      this.editor.setValue(header + localData || header);
+  
     });
 
     this.editor.on('change', () => {
